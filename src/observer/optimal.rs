c3cd20a87ad8ed7180d5ba060c21cc0547e77f10
Gerard Harbers <harbers@gmail.com>
2025-06-19 21:11:52 -0700

    Changed Optimal colors to use a sliding filter only, wrapping around the spectral ends.

--- src/observer/optimal.rs
+++ src/observer/optimal.rs
@@ -10,20 +10,27 @@
 //! functions and a specified reference white, and are useful for visualizing
 //! the limits of color spaces under different viewing conditions.
 
-use std::collections::HashMap;
 use nalgebra::{DMatrix, Vector3};
+use std::collections::HashMap;
 
 use super::Observer;
-use crate::{lab::CieLab, prelude::CieIlluminant, spectrum::NS, xyz::{RelXYZ, XYZ}};
+use crate::{
+    lab::CieLab,
+    prelude::CieIlluminant,
+    spectrum::NS,
+    xyz::{RelXYZ, XYZ},
+};
 
 /// `OptimalColors` represents a matrix of tristimulus values derived from optimal spectra,
 /// which are theoretical reflectance curves that define the outer boundary (gamut limit)
 /// of a color space under a given illuminant and observer.
 ///
-/// The colors are stored in an `NS × NS` matrix, where `NS` is the number of spectral samples.
-/// Each entry is a `Vector3<f64>` containing XYZ values. The upper-left triangle contains
-/// forward optimal colors (monotonically increasing or decreasing reflectance), while
-/// the lower-right triangle contains their inverse counterparts.
+/// The colors are stored in an `NS × NS` matrix, where `NS` is the number of spectral samples.  The
+/// values are computed by integrating blocks of the spectral locus, effectively simulating the
+/// addition of contiguous spectral bands, with increasing width.  Each row in the matrix
+/// corresponds to a different block width, and each column corresponds to a different starting
+/// wavelength in the spectrum.  The block wraps around the spectrum to ensure continuity, and to
+/// represent the full range of possible colors, including purples.
 ///
 /// The accompanying `XYZ` value stores the reference white used in the computations.
 pub struct OptimalColors(XYZ, DMatrix<Vector3<f64>>);
@@ -36,28 +43,27 @@ impl Observer {
     /// * `ref_white` - The reference white illuminant to use for the calculations.
     /// # Returns
     /// * `OptimalColors` - A struct containing the reference white and the matrix of optimal colors.
+    /// # Notes
+    /// * The optimal colors are computed by integrating blocks of the spectral locus,
+    ///   simulating the addition of contiguous spectral bands with increasing width.
+    /// * Each row in the resulting matrix corresponds to a different block width,
+    ///   The block wraps around the spectrum to ensure continuity and to represent the full range of possible colors.
+    /// * This is a theoretical construct and may not correspond to physically realizable colors.
+    /// * As it calculates about 160K colors, it may be computationally intensive.
     pub fn optimal_colors(&self, ref_white: CieIlluminant) -> OptimalColors {
-        let mut optcol: DMatrix<Vector3<f64>> = DMatrix::from_fn(NS, NS, |_, _| Vector3::zeros());
+        let mut optcol: DMatrix<Vector3<f64>> =
+            DMatrix::from_fn(NS - 1, NS, |_, _| Vector3::zeros());
         let spectral_locus = self.spectral_locus(ref_white);
         let white_point = spectral_locus[0].1.white_point();
-        let white_point_vec = white_point.xyz;
 
-        // fill top left, including diagonal, with direct colors
-        for r in 0..NS {
-            for c in 0..NS - r {
-                for w in 0..=r {
-                    optcol[(r, c)] += spectral_locus[c + w].1.xyz().xyz;
-                }
-            }
-        }
-
-        // fill bottom right triangle (inverse colors)
-        for r in 0..NS {
+        for r in 0..NS - 1 {
+            // row
             for c in 0..NS {
-                if r + c <= NS - 1 {
-                    let rr = NS - 1 - r;
-                    let cc = NS - 1 - c;
-                    optcol[(rr, cc)] = white_point_vec - optcol[(r, c)];
+                // column, scroll block through spectrum from blue to red
+                for w in 0..=r {
+                    // integrate block with width r+1
+                    let i = (c + w) % NS; // wrap around
+                    optcol[(r, c)] += spectral_locus[i].1.xyz().xyz;
                 }
             }
         }
@@ -66,6 +72,8 @@ impl Observer {
 }
 
 impl OptimalColors {
+    pub const LAB_MAX_CHROME_LEN: usize = 7128;
+
     pub fn white_point(&self) -> XYZ {
         self.0
     }
@@ -82,52 +90,63 @@ impl OptimalColors {
     ///
     /// Returns a `HashMap` where each key is a `(L, H)` pair:
     /// - `L` is lightness (rounded to the nearest integer in the range 0–100),
-    /// - `H` is the hue angle (divided by 2 and rounded, giving values in 0–180).
+    /// - `H` is the hue category, with the hue angles in 72 bins, each covering 5 degrees.
     ///
     /// Each value in the map is the highest chroma (`C`) observed for that `(L, H)` bin,
-    /// based on the XYZ values stored in the `OptimalColors` matrix.
+    /// based on the XYZ values stored in the `OptimalColors` matrix, and dimmer values to fill the bins.
     ///
-    /// This method is useful for estimating the outer boundary (gamut limit)
-    /// of the LCh color space, especially when visualizing or constraining
-    /// perceptually uniform color representations.
-    pub fn cielab_max_chromas(&self) -> HashMap<(u8, u8), f32> {
+    /// This method is useful for estimating the outer boundary (gamut limit) of the LCh color
+    /// space, especially when visualizing or constraining perceptually uniform color
+    /// representations.
+    pub fn cielab_max_chromas(&self) -> HashMap<(u8, u8), u8> {
         let mut map = HashMap::new();
         let white_point = self.0;
 
-        for xyz in self.1.iter() {
-            let rel_xyz = RelXYZ::from_vec(*xyz, white_point);
+        let mut add_xyz = |xyz: Vector3<f64>, scale: f64| {
+            let xyz_scaled = xyz * scale;
+            let rel_xyz = RelXYZ::from_vec(xyz_scaled, white_point);
             let lab = CieLab::from_xyz(rel_xyz);
-            let [l, chroma, h] = lab.lch();
+            let [l, c, h] = lab.lch();
 
-            let l = l.round() as u8;
-            let h = (h / 2.0).round() as u8;
-            let c_f32 = chroma as f32;
+            let l_u8 = l.round() as u8;
+            let h_u8 = (h / 5.0).floor() as u8;
+            let c_u8 = c.floor() as u8;
 
-            map.entry((l, h))
-                .and_modify(|existing| {
-                    if *existing < c_f32 {
-                        *existing = c_f32;
-                    }
-                })
-                .or_insert(c_f32);
+            if l_u8 > 0 && l_u8 < 100 {
+                map.entry((l_u8, h_u8))
+                    .and_modify(|existing| {
+                        if *existing < c_u8 {
+                            *existing = c_u8;
+                        }
+                    })
+                    .or_insert(c_u8);
+            }
+        };
+
+        // fill the map with the original colors, and then with dimmer versions to cover the full gamut
+        for xyz in self.1.iter() {
+            let mut scale = 1.0;
+            for _ in 1..=7 {
+                add_xyz(*xyz, scale);
+                scale /= 1.61803; // golden ratio
+            }
         }
         map
     }
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::observer::Observer::Cie1931;
     use approx::assert_abs_diff_eq;
+    use strum::IntoEnumIterator;
 
     #[test]
     fn test_optimum_colors() {
         let observer = Cie1931;
         let ref_white = CieIlluminant::D65;
         let opt_colors = observer.optimal_colors(ref_white);
-        let xyz_ref_white = observer.xyz_d65();
 
         let first: Vec<XYZ> = observer
             .spectral_locus(ref_white)
@@ -142,23 +161,11 @@ mod tests {
             .for_each(|(x, y)| {
                 assert_abs_diff_eq!(x, y, epsilon = 1e-10);
             });
-        
-        let last: Vec<XYZ> = observer
-            .spectral_locus(ref_white)
-            .iter()
-            .rev()
-            .map(|(_w, xyz)| xyz_ref_white - xyz.xyz())
-            .collect();
 
-        last 
-            .iter()
-            .map(|xyz| xyz.xyz)
-            .zip(opt_colors.colors().row(NS-1).iter())
-            .for_each(|(x, y)| {
-                println!("{:.5} {:.5} {:.5} {:.5} {:.5} {:.5}", x.x, x.y, x.z, y.x, y.y, y.z);
-                assert_abs_diff_eq!(x, y, epsilon = 1e-10);
-            });
-
+        dbg!(
+            opt_colors.colors().row(NS - 2)[0],
+            opt_colors.colors().row(NS - 2)[NS - 1]
+        );
     }
 
     #[test]
@@ -167,15 +174,30 @@ mod tests {
         let ref_white = CieIlluminant::D65;
         let opt_colors = observer.optimal_colors(ref_white);
         let cielch = opt_colors.cielab_max_chromas();
-        for l in 0..=100 {
-            for h in 0..=180 {
-                if let Some(c) = cielch.get(&(l, h)) {
-                    println!("{}, {}, {}", l, h, c);
+        for l in 1..=99 {
+            for h in 0..72 {
+                if cielch.get(&(l, h)).is_none() {
+                    eprintln!("bin {}, {} not populated", l, h);
                 }
             }
-        }   
+        }
+        assert_eq!(cielch.len(), OptimalColors::LAB_MAX_CHROME_LEN);
+    }
+
+    #[test]
+    fn test_cielch_all_observers() {
+        for ref_white in [CieIlluminant::D65, CieIlluminant::D50] {
+            for obs in Observer::iter() {
+                let opt_colors = obs.optimal_colors(ref_white);
+                let cielch = opt_colors.cielab_max_chromas();
+                assert_eq!(
+                    cielch.len(),
+                    OptimalColors::LAB_MAX_CHROME_LEN,
+                    "Observer: {}, Reference White: {:?} has incorrect cielch length",
+                    obs.as_ref(),
+                    ref_white
+                );
+            }
+        }
     }
 }
-
-
-
