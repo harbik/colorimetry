9d9e3bb7f340621b1b7639543ee6afdc2eac0102
Gerard Harbers <harbers@gmail.com>
2025-07-19 09:15:19 +0200

    Rename color CLI tool to colorpoint; update file paths and enhance output formatting

--- bin/colorpoint.rs
+++ bin/colorpoint.rs
@@ -2,20 +2,19 @@
 
 use colorimetry::lab::CieLab;
 use colorimetry::{
-    colorant::Colorant, illuminant::D65, observer::Observer, rgb::RgbSpace,
-    spectrum::Spectrum,
+    colorant::Colorant, illuminant::D65, observer::Observer, rgb::RgbSpace, spectrum::Spectrum,
 };
 
-use std::env;
-use std::fs::File;
-use std::error::Error;
 use csv::ReaderBuilder;
+use std::env;
+use std::error::Error;
+use std::fs::File;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let path = env::args().nth(1).expect("Usage: color <file.csv>");
     let file = File::open(path)?;
     let mut rdr = ReaderBuilder::new()
-        .comment(Some(b'#'))  // 👈 ignore lines starting with '#'
+        .comment(Some(b'#')) // 👈 ignore lines starting with '#'
         .from_reader(file);
 
     let mut wavelengths = Vec::new();
@@ -34,7 +33,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     let xyz = Observer::Cie1931.rel_xyz(&D65, &colorant);
     let lab = CieLab::from_rxyz(xyz);
     let rgb: [u8; 3] = xyz.xyz().rgb(RgbSpace::SRGB).compress().into();
-    
-    println!("{xyz:?} {lab:?} {rgb:?}");
+
+    let [x, y, z] = xyz.xyz().values();
+    let xyz = format!("\t{x:.2} {y:.2} {z:.2}");
+    let lab = format!("{:.2} {:.2} {:.2}", lab.l(), lab.a(), lab.b());
+    let rgb = format!("\t{:3} {:3} {:3}", rgb[0], rgb[1], rgb[2]);
+    println!("XYZ: {xyz}\nCieLab: {lab}\nsRGB: {rgb}");
     Ok(())
 }
